ea9d9b687dbae42b4e8bd7936d9ae3aae47532ff
Nikhil <nikhilganireddy@yahoo.com>
2023-05-16 03:20:01 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://rudramadevi.onrender.com"]
+    origin: ["http://localhost:5173", "https://rudramadevi.netlify.app"]
   })
 );
 app.use(cookieparser());
