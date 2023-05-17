157c125613179bdb742a2d5dba3a34019c831545
Nikhil <nikhilganireddy@yahoo.com>
2023-05-17 19:22:39 +0530

    mn mn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cookieparser = require("cookie-parser");
+const multer = require("multer");
+require("dotenv").config();
 
 const User = require("./models/User");
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(express.json());
 app.use(
   cors({
@@ -37,7 +39,8 @@ app.post("/login", async (req, res) => {
           pic: userDoc.pic,
           branch: userDoc.branch,
           year: userDoc.year,
-          room: userDoc.room
+          room: userDoc.room,
+          password: userDoc.password
         },
         jwtsecret,
         {},
@@ -60,6 +63,24 @@ app.get("/profile", (req, res) => {
       res.json({ name, hallTicket, id, pic, branch, year, room });
     });
   } else res.json(null);
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+
+const photosMiddlewear = multer({
+  dest: "uploads/"
+});
+
+app.post("/upload", photosMiddlewear.array("photo", 1), (req, res) => {
+  console.log(req.files);
+  res.json(req.files);
+});
+
+app.post("/user/change-password", (req, res) => {
+  const { currentPassword, newPassword, confirmNewPassword } = req.body;
+  res.json({ currentPassword, newPassword, confirmNewPassword });
 });
 
 app.listen(port);
