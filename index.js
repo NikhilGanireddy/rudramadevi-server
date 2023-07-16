67c94de92f04c78419a80af453c5047b2435ed31
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-16 18:30:25 +0530

    new commit roi

--- index.js
+++ index.js
@@ -12,74 +12,80 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(
-  cors({
-    credentials: true,
-    origin: ["https://rudramadevi.netlify.app", "http://localhost:5173"],
-  })
+    cors({
+        credentials: true,
+        origin: ["https://rudramadevi.netlify.app",
+            // "http://localhost:5173"
+        ],
+    })
 );
 app.use(cookieparser());
 
 mongoose.connect(process.env.MONGOURL);
 const jwtsecret = "lkdsfnbwlkskjthwkvebtrwldlnmvnsldzgcnxlc";
 const photosMiddlewear = multer({
-  dest: "uploads/",
+    dest: "uploads/",
 });
 
 app.get("/test", (req, res) => {
-  res.json("server is working");
+    res.json("server is working");
 });
 
 app.post("/login", async (req, res) => {
-  const { name, hallTicket, password } = req.body;
-  const userDoc = await User.findOne({ hallTicket });
-  if (userDoc) {
-    if (userDoc.name == name && userDoc.password == password) {
-      jwt.sign(
-        {
-          name: userDoc.name,
-          id: userDoc._id,
-          hallTicket: userDoc.hallTicket,
-          pic: userDoc.pic,
-          branch: userDoc.branch,
-          year: userDoc.year,
-          room: userDoc.room,
-          password: userDoc.password,
-        },
-        jwtsecret,
-        {},
-        (err, token) => {
-          if (err) throw err;
-          else res.cookie("token", token).json(userDoc);
+
+    // await User.create({
+    //     name: "nikhil", hallTicket: "20017T0901", password: "12345678"
+    // })
+    const {name, hallTicket, password} = req.body;
+    const userDoc = await User.findOne({hallTicket});
+    if (userDoc) {
+        if (userDoc.name == name && userDoc.password == password) {
+            jwt.sign(
+                {
+                    name: userDoc.name,
+                    id: userDoc._id,
+                    hallTicket: userDoc.hallTicket,
+                    pic: userDoc.pic,
+                    branch: userDoc.branch,
+                    year: userDoc.year,
+                    room: userDoc.room,
+                    password: userDoc.password,
+                },
+                jwtsecret,
+                {},
+                (err, token) => {
+                    if (err) throw err;
+                    else res.cookie("token", token).json(userDoc);
+                }
+            );
         }
-      );
-    }
-  } else res.json(`User with the hallticket ${hallTicket} is not found`);
+    } else res.json(`User with the hallticket ${hallTicket} is not found`);
 });
 
 app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  if (token) {
-    jwt.verify(token, jwtsecret, {}, async (err, tokendata) => {
-      if (err) throw err;
-      const { name, hallTicket, id, pic, branch, year, room } =
-        await User.findById(tokendata.id);
-      res.json({ name, hallTicket, id, pic, branch, year, room });
-    });
-  } else res.json(null);
+    const {token} = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtsecret, {}, async (err, tokendata) => {
+            if (err) throw err;
+            const {name, hallTicket, id, pic, branch, year, room} =
+                await User.findById(tokendata.id);
+            res.json({name, hallTicket, id, pic, branch, year, room});
+        });
+    } else res.json(null);
 });
 
 app.post("/logout", (req, res) => {
-  res.cookie("token", "").json(true);
+    res.cookie("token", "").json(true);
 });
 
 app.post("/upload", photosMiddlewear.array("photo", 1), (req, res) => {
-  console.log(req.files);
-  res.json(req.files);
+    console.log(req.files);
+    res.json(req.files);
 });
 
 app.post("/user/change-password", (req, res) => {
-  const { currentPassword, newPassword, confirmNewPassword } = req.body;
-  res.json({ currentPassword, newPassword, confirmNewPassword });
+    const {currentPassword, newPassword, confirmNewPassword} = req.body;
+    res.json({currentPassword, newPassword, confirmNewPassword});
 });
 
 app.listen(port);
